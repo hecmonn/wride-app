bd8bb918dd14da1edbca566d485b05caee84f8d1
hecmonn <hecmonn@gmail.com>
2018-04-02 19:35:01 -0500

    refactored follow btn

---
+++ src/components/Profile/Followers/FollowerBtn.js
@@ -0,0 +1,58 @@
+import React, { PropTypes } from 'react';
+import {Button,Text} from 'native-base';
+import {View} from 'react-native';
+
+class FollowerBtn extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            followLabel: this.props.following?'Following':'Follow',
+            unFollowLoading: false,
+            following: this.props.following
+        }
+    }
+
+    followerBtn=()=>{
+        const {following,followLabel,unFollowLoading}=this.state;
+        console.log('following: ',following);
+        return(
+            <Button small style={{backgroundColor:'white',borderColor:following?'#000aff':'#757575',borderWidth:1,borderRadius:5}} onPress={()=>this.unFollow()}>
+                <Text style={{color:following?'#000aff':'#757575', fontWeight:'bold'}}>{unFollowLoading?'loading...':followLabel}</Text>
+            </Button>
+        )
+    }
+    unFollow=()=>{
+        const {username,username_param}=this.props.users;
+        const {following}=this.state;
+        this.setState({unFollowLoading:true})
+        this.props.getUnFollow({username,username_param,following})
+        .then(r=>{
+            this.setState({followLabel:r.data.action,unFollowLoading:false,following:r.data.following});
+        })
+    }
+
+    editBtn=(
+        <Button small
+            style={{backgroundColor:'white',borderColor: '#181818',borderWidth:1,borderRadius:5}}
+            onPress={()=>this.props.navigation.navigate('Settings')}>
+                <Text style={{color:'black', fontWeight:'bold'}}>Edit Profile</Text>
+        </Button>
+    )
+    
+    componentWillReceiveProps(nextProps){
+        if(nextProps.following!==this.props.following){
+            this.setState({following:nextProps.following,followLabel:nextProps.following?'Following':'Follow'});
+        }
+    }
+    render () {
+        const {ownProfile}=this.props;
+        console.log('Following: ',this.props.following)
+        return(
+            <View>
+                {ownProfile?this.editBtn: this.followerBtn()}
+            </View>
+        )
+    }
+}
+
+export default FollowerBtn;
--- src/components/Profile/Who.js
+++ src/components/Profile/Who.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react'
 import {Content,Left,Right,Body,Title,Thumbnail,Text,H1,Button,Icon,Container} from 'native-base';
 import {Grid,Col,Row} from 'react-native-easy-grid';
 import ImageModal from 'react-native-image-view';
+import FollowerBtn from './Followers/FollowerBtn';
 
 class Who extends React.Component {
     constructor(props){
@@ -15,34 +16,6 @@ class Who extends React.Component {
         };
     }
 
-    unFollow=()=>{
-        const {username}=this.props;
-        const {following}=this.state;
-
-        const username_param=this.props.person.username;
-        this.setState({unFollowLoading:true})
-        this.props.getUnFollow({username,username_param,following})
-        .then(r=>{
-            this.setState({followLabel:r.data.action,unFollowLoading:false,following:r.data.following});
-        })
-    }
-
-    followerBtn=()=>{
-        const {following,followLabel,unFollowLoading}=this.state;
-        return(
-            <Button small style={{backgroundColor:'white',borderColor:following?'#000aff':'#757575',borderWidth:1,borderRadius:5}} onPress={()=>this.unFollow()}>
-                <Text style={{color:following?'#000aff':'#757575', fontWeight:'bold'}}>{unFollowLoading?'loading...':followLabel}</Text>
-            </Button>
-        )
-    }
-    editBtn=(
-        <Button small
-            style={{backgroundColor:'white',borderColor: '#181818',borderWidth:1,borderRadius:5}}
-            onPress={()=>this.props.navigation.navigate('Settings')}>
-                <Text style={{color:'black', fontWeight:'bold'}}>Edit Profile</Text>
-        </Button>
-    )
-
     componentWillReceiveProps(nextProps){
         if(nextProps.following!==this.props.following){
             this.setState({following:nextProps.following,followLabel:nextProps.following?'Following':'Follow'});
@@ -51,6 +24,7 @@ class Who extends React.Component {
     render () {
         const {navigation,ownProfile,following}=this.props;
         const {name,username,bio,path}=this.props.person;
+        const {followLabel,unFollowLoading}=this.state
         let uri=`http://localhost:5005/${this.state.path}`;
 
         const images=[{source:{uri:`http://localhost:5005/${this.state.path}`}}];
@@ -78,7 +52,7 @@ class Who extends React.Component {
                     <Row>
                         <Col size={2} />
                         <Col size={1}>
-                            {ownProfile?this.editBtn: this.followerBtn()}
+                            <FollowerBtn followProps={ownProfile} following={this.state.following} navigation={navigation} users={{username:this.props.username,username_param:this.props.person.username}} getUnFollow={this.props.getUnFollow}/>
                         </Col>
                     </Row>
                 </Grid>
