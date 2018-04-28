f54d3a4ced37de75a13274a70f073b5d35b922d7
hecmonn <hecmonn@gmail.com>
2018-04-28 09:53:18 -0700

    missing un-cta on inspiration modal

--- src/components/Inspiration/Card.js
+++ src/components/Inspiration/Card.js
@@ -13,20 +13,54 @@ class InspirationCard extends React.Component {
         this.state={
             visible_modal:false,
             post_visible_modal:false,
+            shared:this.props.item.item.shared,
+            liked:this.props.item.item.liked,
+            saved:this.props.item.item.saved,
+            likes_cnt:this.props.item.item.likes_cnt,
+            shares_cnt:this.props.item.item.shares_cnt
         }
     }
 
-    render () {
+    actionPost=(action)=>{
+        const {id,username}=this.props.item.item;
+        const {auser}=this.props;
+        const{liked,shared,likes_cnt,shares_cnt,saved}=this.props.item.item;
+        this.props.postAction({id,auser,action,liked,shared,saved})
+        .then(r=>{
+            switch(r.data.wydn){
+                case 'liked':
+                    this.setState({liked:1,likes_cnt:likes_cnt+1});
+                    break;
+                case 'shared':
+                    this.setState({shared:1,shares_cnt:shares_cnt+1});
+                    break;
+                case 'saved':
+                    this.setState({saved:1});
+                    break;
+                case 'unliked':
+                    this.setState({liked:0,likes_cnt:likes_cnt-1});
+                    break;
+                case 'unshared':
+                    this.setState({shared:0,shares_cnt:shares_cnt-1});
+                    break;
 
-        console.log('Post: ',this.props.item.item);
+                case 'unsaved':
+                    this.setState({saved:0});
+                    break;
+                default: this.setState({liked,shared,saved});
+            }
+        });
+    }
+
+    render () {
         const {anonymous,id,title,post_path,username}=this.props.item.item;
-        const modalContent={...this.props.item.item};
-        const {post_visible_modal,visible_modal}=this.state;
-        const {auser,postAction}=this.props;
+        const {post_visible_modal,visible_modal,liked,shared,saved,likes_cnt,shares_cnt}=this.state;
+        const modalContent={...this.props.item.item,liked,shared,saved,likes_cnt,shares_cnt};
+        const {auser}=this.props;
         return (
             <Card>
                 <OptionsModal opts={{id,own_post:auser==username,username,auser}} visible={visible_modal} hideModal={()=>this.setState({visible_modal:false})}/>
-                <Modal content={modalContent} visible={post_visible_modal} actionPost={actionPost} postAction={this.props.postAction} hideModal={()=>this.setState({post_visible_modal:false})} />
+                <Modal content={modalContent} visible={post_visible_modal} actionPost={this.actionPost} postAction={this.props.postAction} hideModal={()=>this.setState({post_visible_modal:false})} />
                 <CardItem button onPress={()=>this.setState({post_visible_modal:true})}>
                     <Left>
                         <Text style={{fontWeight:'bold'}}>{title}</Text>
